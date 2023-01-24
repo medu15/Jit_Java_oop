d76976cae9b5e14e13015f279ba62686109c9158
medu15 <123404119+medu15@users.noreply.github.com>
2023-01-24 09:57:17 -0800

    Add files via upload

---
+++ exersice 2/src/many/bike1.java
@@ -0,0 +1,36 @@
+
+package many;
+
+
+public class bike1{ 
+  
+bike1()
+        
+{
+System.out.println("Bike is created");
+}
+public int add(int a,int b)
+        
+{
+    int sum=a+b;
+    return sum;
+}
+
+  
+public static void main(String args[]){  
+ 
+bike1 b=new bike1();
+int num1=24;
+int num2=15;
+int result=b.add(num1,num2);
+System.out.println(result);
+}  
+}   
+
+
+
+
+        
+    
+    
+
